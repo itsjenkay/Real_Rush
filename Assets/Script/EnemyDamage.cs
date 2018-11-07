bab1b19b75aa42004eaeddc69a60c5f62aeb5808
uduji chikordi <udujichikordi@gmail.com>
2018-11-07 20:41:21 +0100

    9_Creating_Enemy_Hit_points

--- Assets/Script/EnemyDamage.cs
+++ Assets/Script/EnemyDamage.cs
@@ -3,15 +3,34 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class EnemyDamage : MonoBehaviour {
+    [SerializeField] int hitPoints = 10;
     [SerializeField] Collider collisionMesh;
 	// Use this for initialization
 	void Start () {
-        print("hi");
+       
     }
 	
 	private void OnParticleCollision(GameObject other)
     {
-        print("i am hit");
+        ProcessHits();
+        if (hitPoints <= 1)
+        {
+            killenemy();
+        }
+      
     }
+
     
+
+    public void ProcessHits()
+    {
+        hitPoints = hitPoints - 1;
+        print("current hitpoints is " + hitPoints);
+
+    }
+    private void killenemy()
+    {
+        Destroy(gameObject, 0.5f);
+    }
+
 }
